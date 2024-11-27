62cc65f796a80f0a171040d4649c93fb5414157e
isaacmartin <isaacissamartin@gmail.com>
2024-11-27 01:58:11 -0500

    new code

--- main.py
+++ main.py
@@ -16,11 +16,19 @@ try:
     # print("Successfully connected to Oracle Database")
     cursor = connection.cursor()
 
-    movie_db_list = [row for row in cursor.execute("SELECT * FROM movie")]
+    actor_db_list = [row for row in cursor.execute("SELECT * FROM actor")]
     customer_db_list = [row for row in cursor.execute("SELECT * FROM customer")]
     membership_db_list = [row for row in cursor.execute("SELECT * FROM membership")]
     admin_db_list = [row for row in cursor.execute("SELECT * FROM admin")]
     payment_db_list = [row for row in cursor.execute('SELECT * FROM payment')]
+    movie_db_list = [row for row in cursor.execute("SELECT * FROM movie")]
+    account_db_list = [row for row in cursor.execute("SELECT * FROM account")]
+
+
+
+
+
+
 
 
 
@@ -38,13 +46,29 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
 ## TIME to start putting shit in the UI
+
+@app.route('/',methods=['GET','POST'])
+def sign_in():
+    if request.method =='POST':
+        USERNAME = request.form['username']
+        PASSWORD = request.form['password']
+
+        for account in account_db_list:
+            if account[1] == USERNAME and account[2] == PASSWORD:
+                return redirect('home')
+
+    return render_template('loginpage.html',account_db_list=account_db_list)
+
 @app.route('/home')
 def edit():
-    return render_template('home.html')
+    return render_template('home.html',movie_db_list=movie_db_list)
 
-@app.route('/')
-def sign_in():
-    return render_template('sign.html')
+@app.route('/movie')
+def movie():
+
+    return render_template('movie.html')
+
+
 
 
 if __name__ == '__main__':
